ebf60cfcf754e9f44d0000abffcb57152ed23339
oleg <oleg.brovchenkov@gmail.com>
2024-10-14 21:35:25 +0200

    Setup

---
+++ Assets/RecycleFactory/Global/Scripts.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Reflection;
+#endif
+
+[ExecuteInEditMode]
+public class Scripts : MonoBehaviour
+{
+    private void OnValidate()
+    {
+#if UNITY_EDITOR
+        if (EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            if (HasNullSerializedFields())
+            {
+                Debug.LogError("Play mode prevented due to null fields in Scripts.");
+                EditorApplication.isPlaying = false;
+            }
+        }
+#endif
+    }
+
+    private bool HasNullSerializedFields()
+    {
+#if UNITY_EDITOR
+        var fields = GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        foreach (var field in fields)
+        {
+            // Check only serialized fields (public or with [SerializeField])
+            bool isSerializable = field.IsPublic || field.GetCustomAttribute<SerializeField>() != null;
+            if (isSerializable && field.GetValue(this) == null)
+            {
+                Debug.LogError($"Null field detected: {field.Name}");
+                return true;
+            }
+        }
+#endif
+        return false;
+    }
+}
