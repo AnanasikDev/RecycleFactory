ca09fc2d772e7310bda332252d2c10d4c9b5de5b
oleg <oleg.brovchenkov@gmail.com>
2024-12-20 16:03:06 +0100

    Buildings costs adjusted; Ferromagnetic sorter animations added; SortingMachineAnimator_Employee improved

--- Assets/RecycleFactory/Buildings/SortingMachineAnimator_Employee.cs
+++ Assets/RecycleFactory/Buildings/SortingMachineAnimator_Employee.cs
@@ -1,13 +1,21 @@
-﻿using RecycleFactory.Buildings.Logistics;
+﻿using NaughtyAttributes;
+using RecycleFactory.Buildings.Logistics;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.LightTransport.PostProcessing;
 
 namespace RecycleFactory.Buildings
 {
     internal class SortingMachineAnimator_Employee : SortingMachineAnimator
     {
         [SerializeField] private Animator animator;
-        [SerializeField] private Transform handTransform;
+        [SerializeField] private bool attachToOneTransform = true;
+        [SerializeField][ShowIf("attachToOneTransform")] private Transform singleParent;
+
+        [SerializeField][HideIf("attachToOneTransform")][Tooltip("An array of transforms, average position of which represent an item position")] private Transform[] averageHandlers;
+
+        private bool isAnimating = false;
 
         private ConveyorBelt_Item item;
         private int anchorIndex;
@@ -18,14 +26,32 @@ namespace RecycleFactory.Buildings
             animator = GetComponent<Animator>();
         }
 
+        private void Update()
+        {
+            if (!isAnimating || item == null) return;
+
+            if (!attachToOneTransform)
+            {
+                Vector3 pos = Vector3.zero;
+                foreach (var handler in averageHandlers)
+                {
+                    pos += handler.position;
+                }
+                item.transform.position = pos / averageHandlers.Length;
+            }
+        }
+
         public override void OnReceive(ConveyorBelt_Item item, int anchorIndex)
         {
             isReadyToReceive = false;
             this.item = item;
             this.anchorIndex = anchorIndex;
-            item.transform.SetParent(handTransform);
+            if (attachToOneTransform)
+                item.transform.SetParent(singleParent);
+            else
             item.transform.localPosition = Vector3.zero;
             animator.SetTrigger("forward");
+            isAnimating = true;
         }
 
         public override void OnRelease()
@@ -33,6 +59,7 @@ namespace RecycleFactory.Buildings
             this.item = null;
             this.anchorIndex = -1;
             animator.SetTrigger("backward");
+            isAnimating = true;
         }
 
         public override void Receive2ReleaseAnimEnded()
@@ -40,6 +67,7 @@ namespace RecycleFactory.Buildings
             Debug.Log("Receive2Release anim ended");
             //onReadyToReleaseEvent?.Invoke(item, anchorIndex);
 
+            isAnimating = false;
             IEnumerator tryUntil()
             {
                 // try to release until succeed
@@ -54,6 +82,7 @@ namespace RecycleFactory.Buildings
         
         public override void Release2ReceiveAnimEnded()
         {
+            isAnimating = false;
             Debug.Log("Release2Receive anim ended");
             isReadyToReceive = true;
         }
